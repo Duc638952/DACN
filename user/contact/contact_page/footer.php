3b5f92270e0f29dbd1cb397a2ffe0c8d8c3259d7
tqminh.0907 <tqminh.0907@gmail.com>
2021-12-18 21:11:50 +0700

    xây dựng trang contact

---
+++ user/contact/contact_page/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <ul>
+        <li><a href="">Contact</a></li>
+        <li><a href="">Shipping</a></li>
+        <li><a href="">Terms</a></li>
+        <li><a href="">Sign in</a></li>
+    </ul>
+    <ul id="tt">
+        <li>Copyright © 2021 DONCARE STUDIO. Powered by Tylow</li>
+    </ul>
+</div>
---
+++ user/contact/contact_page/search.php
@@ -0,0 +1,6 @@
+<div class="search-box">
+        <input class="search-txt" type="text" name=" " placeholder="Search...">
+        <a class="search-btn" href="#">
+            <li class="ti-search"></li> 
+        </a>
+</div>
